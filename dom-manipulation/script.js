a84bdd7c2b8787e10708c7b2c5aef82913339016
sadeemmuk <sadeemmuk2012@gmail.com>
2024-07-28 14:16:46 +0300

    updated html and js files

---
+++ dom-manipulation/script.js
@@ -0,0 +1,48 @@
+
+let quotes = [
+    {
+        qoute:"to be or not to be that is the question",
+        category:10
+    },
+    {
+        qoute:"Teach me and I will forget, Engage me and I will remember",
+        category:3
+    },
+    {
+        qoute:"Life is a game there are winner and losser all the time",
+        category:2
+    }
+]
+
+let newQuote = document.getElementById('newQuote');
+
+newQuote.addEventListener('click', showRandomQuote);
+
+
+function addQuote(){
+    let newQuoteText = document.getElementById('newQuoteText');
+    let newCatogery = document.getElementById('newQuoteCategory');
+
+    if(newQuoteText.value !== "" && newCatogery.value !== ""){
+    let hello = {
+        qoute: newQuoteText.value,
+        category: newCatogery.value
+    }
+    quotes.push(hello);
+    
+
+    newQuoteText.value = "";
+    newCatogery.value = "";
+    }
+}
+
+
+function showRandomQuote(){
+    let q = document.getElementById('quoteDisplay');
+ 
+//   console.log(quotes.qoute[Math.random(0, 3)]);
+q.textContent= quotes[Math.floor(Math.random() * quotes.length)].qoute;
+
+addQuote();
+
+}
