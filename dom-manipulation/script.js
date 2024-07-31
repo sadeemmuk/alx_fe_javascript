fe00cff29db398716d39f1bf4e4f5ea40a27a7a8
sadeemmuk <sadeemmuk2012@gmail.com>
2024-07-31 11:10:20 +0300

    updated files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,52 +1,39 @@
-
-
-let quotes = [
-    {
-        text:"to be or not to be that is the question",
-        category:10
-    },
-]
-
 let newQuote = document.getElementById('newQuote');
 
 newQuote.addEventListener('click', showRandomQuote);
-
+let newQuoteText = document.getElementById('newQuoteText');
+let newCatogery = document.getElementById('newQuoteCategory');
 
 function addQuote(){
-    let newQuoteText = document.getElementById('newQuoteText');
-    let newCatogery = document.getElementById('newQuoteCategory');
 
     if(newQuoteText.value !== "" && newCatogery.value !== ""){
-    let createAddQuoteForm = {
-        text: newQuoteText.value,
-        category: newCatogery.value
-    }
+    // let createAddQuoteForm = {
+    //     text: newQuoteText.value,
+    //     category: newCatogery.value
+    // }
 
-    quotes.push(createAddQuoteForm);
-    window.localStorage.setItem('text', createAddQuoteForm.text);
-    window.localStorage.setItem('category', createAddQuoteForm.category);
-    
-
+    // quotes.push(createAddQuoteForm);
+    window.localStorage.setItem(newQuoteText.value, newCatogery.value);
 
     newQuoteText.value = "";
     newCatogery.value = "";
     }
+
 }
+
+
 
 
 function showRandomQuote(){
     let q = document.getElementById('quoteDisplay');
-    
 
- newOne = quotes[Math.floor(Math.random() * quotes.length)].text;
- newCat =quotes[Math.floor(Math.random() * quotes.length)].category;
-
-q.innerHTML = `"${newOne}" ${newCat}`;
-
+var keys = Object.keys(localStorage);
+q.innerHTML = keys[Math.floor(Math.random() * keys.length)];
 
 addQuote();
 
 }
+
 
 function importFromJsonFile(event) {
     const fileReader = new FileReader();
